5eea536e326604edd2ddf0245026c396776bfe3c
Supreeth-P <61347631+Supreeth-P@users.noreply.github.com>
2024-08-05 22:11:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.title("🔎 LangChain - Chat with search")
 """
 # In this example, we're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
 # Streamlit Agent examples at [github.com/langchain-ai/streamlit-agent](https://github.com/langchain-ai/streamlit-agent)
-
+##########################################################################################################################################
 st.sidebar.title("Settings")
 api_key = st.sidebar.text_input("Enter your Groq API Key:",type = "password")
 
